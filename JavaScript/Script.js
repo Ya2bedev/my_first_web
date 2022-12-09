461b3d6f8f50a753775fe9a4ef7bf9c37fef74fd
chhaya <brocyrip@gmail.com>
2022-12-09 18:30:48 +0700

    Initailize

---
+++ JavaScript/Script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  const date = new Date();
+  const hour = date.getHours();
+  let message = " ";
+  if (hour <= 12) {
+    message = "Good Moring";
+  }
+  if (hour > 12) {
+    message = "Good Afternoon";
+  }
+  if (hour > 17) {
+    message = "Good Evening";
+  }
+  $("#GOOD")
+    .empty()
+    .append( " '' " + message + " '' " + " From CHHAYA");
+});
